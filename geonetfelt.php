dc8a1b6b988bbec01d6a2a81fb0fabe78f42b845
Sam Russell <sam.h.russell@gmail.com>
2013-07-22 19:51:04 +1200

    missing a dot

--- geonetfelt.php
+++ geonetfelt.php
@@ -9,7 +9,7 @@ header('Access-Control-Allow-Origin: *');
             <title>Geonet Reader</title>
             <link rel="stylesheet" href="geonet.css" />
             <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-            <script type="text/javascript" src="geonetfeltjs.php"></script>
+            <script type="text/javascript" src="geonetfelt.js.php"></script>
         </head>
 <body>
 
