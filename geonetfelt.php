2183b173cc8ef7909c9ad71455013266603029a5
Sam Russell <sam.h.russell@gmail.com>
2013-07-22 19:48:22 +1200

    quick hack to do felt earthquakes

---
+++ geonetfelt.php
@@ -0,0 +1,21 @@
+<?
+//header('Access-Control-Allow-Origin: http://www.geonet.org.nz');
+//header('Access-Control-Allow-Origin: http://ajax.googleapis.com');
+header('Access-Control-Allow-Origin: *');
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Geonet Reader</title>
+            <link rel="stylesheet" href="geonet.css" />
+            <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script type="text/javascript" src="geonetfeltjs.php"></script>
+        </head>
+<body>
+
+    <div class="quakeContainer">
+        <ul id="eq"></ul>
+    </div>
+
+</body>
+</html>
