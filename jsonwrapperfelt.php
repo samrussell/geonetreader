44df4423c23c70bab732d997601d720931b8cb7a
Sam Russell <sam.h.russell@gmail.com>
2013-07-22 19:52:13 +1200

    got wrappers mixed up

--- jsonwrapperfelt.php
+++ jsonwrapperfelt.php
@@ -1,5 +1,5 @@
 <?
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-echo file_get_contents("http://www.geonet.org.nz/quakes/services/all.json");
+echo file_get_contents("http://www.geonet.org.nz/quakes/services/felt.json");
 ?>
